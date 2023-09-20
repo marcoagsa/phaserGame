b60385eb19f17dfef7012fe77f317a6e54395e59
Marco Sa <marco.sa@md3.pt>
2023-09-20 19:32:22 +0100

    test gifs

--- src/game/MainScene.ts
+++ src/game/MainScene.ts
@@ -24,7 +24,8 @@ export class MainScene extends Scene {
   }
 
   preload() {
-    this.load.image('star', 'assets/star.png');
+    // this.load.image('star', 'assets/star.png');
+    this.load.image('star', 'assets/starGif.gif');
     this.load.image('bomb', 'assets/bomb.png');
     this.load.image('platform', 'assets/platform.png');
     this.load.image('leftArrow', 'assets/leftarrow.png');
@@ -35,7 +36,7 @@ export class MainScene extends Scene {
     });
   }
 
-  create() {
+  initValues() {
     this.screenWidth = this.scale.width;
     this.screenHeight = this.scale.height;
     this.screenCenterX = this.screenWidth / 2;
@@ -60,7 +61,9 @@ export class MainScene extends Scene {
       .image(this.screenWidth * 0.7, this.gameAreaHeight + 40, 'rightArrow')
       .setOrigin(0, 0)
       .setInteractive();
+  }
 
+  addPlayerAnimation() {
     // adds animations for player
     if (!this.anims.exists('left')) {
       this.anims.create({
@@ -90,10 +93,9 @@ export class MainScene extends Scene {
     // sets player physics
     this.player.body.setGravityY(300);
     this.player.setCollideWorldBounds(true);
+  }
 
-    // adds collider between player and platforms
-    this.physics.add.collider(this.player, this.platform);
-
+  addPlayerMoves() {
     this.moveLeft = false;
     this.moveRight = false;
 
@@ -112,48 +114,130 @@ export class MainScene extends Scene {
     this.rightArrow.on('pointerup', () => {
       this.moveRight = false;
     });
+  }
 
+  async addStars() {
     // Adds generated stars
-
     this.stars = this.physics.add.group({
       gravityY: 300,
     });
 
-    const createStar = () => {
-      const x = Math.random() * this.screenWidth;
-      const star = this.stars.create(x, 0, 'star');
-    };
+    this.createStar();
+    this.createStarLoop();
+  }
 
-    const createStarLoop = this.time.addEvent({
-      // random number between 1 and 1.2 seconds
-      delay: Math.floor(Math.random() * (1200 - 1000 + 1)) + 1000,
-      callback: createStar,
+  createStar() {
+    const x = Math.random() * this.screenWidth;
+    const star = this.stars.create(x, 0, 'star');
+    star.setScale(0.1);
+  }
+
+  createStarLoop() {
+    const delay = Math.floor(Math.random() * (1200 - 1000 + 1)) + 1000;
+
+    const event = this.time.addEvent({
+      delay: delay,
+      callback: this.createStar,
       callbackScope: this,
       loop: true,
     });
+  }
 
+  addBooms() {
     // Adds generated bombs
-
     this.bombs = this.physics.add.group({
       gravityY: 900,
     });
 
-    const createBomb = () => {
-      const x = Math.random() * this.screenWidth;
-      const bomb = this.bombs.create(x, 0, 'bomb');
-      bomb.setScale(2).refreshBody();
-    };
+    this.createBomb();
+    this.createBombLoop();
+  }
 
-    const createBombLoop = this.time.addEvent({
+  createBomb() {
+    const x = Math.random() * this.screenWidth;
+    const bomb = this.bombs.create(x, 0, 'bomb');
+    bomb.setScale(2).refreshBody();
+  }
+
+  createBombLoop() {
+    const delay = Math.floor(Math.random() * (5000 - 4500 + 1)) + 4500;
+
+    const event = this.time.addEvent({
       // random number between 4.5 and 5 seconds
       delay: Math.floor(Math.random() * (5000 - 4500 + 1)) + 4500,
-      callback: createBomb,
+      callback: this.createBomb,
       callbackScope: this,
       loop: true,
     });
+  }
+
+  playerAndStars() {
+    // Adds overlap between player and stars
+    this.score = 0;
+    this.scoreText = this.add
+      .text(this.screenCenterX, this.gameAreaHeight + 16, 'Score: 0', {
+        fontSize: '16px',
+        color: '#000',
+      })
+      .setOrigin(0.5, 0.5);
+
+    this.physics.add.overlap(
+      this.player,
+      this.stars,
+      (object1: any, object2: any) => {
+        const star = object1.key === 'player' ? object1 : object2;
+        star.destroy();
+        this.score += 10;
+        this.player.scale += 0.1;
+        this.scoreText.setText('Score: ' + this.score);
+      },
+      undefined,
+      this
+    );
+  }
+
+  playerAndBooms() {
+    // Adds overlap between player and bombs
+    this.physics.add.overlap(
+      this.player,
+      this.bombs,
+      (object1: any, object2: any) => {
+        const bomb = object1.key === 'player' ? object1 : object2;
+        bomb.destroy();
+        this.time.removeAllEvents();
+        this.physics.pause();
+
+        this.gameOverText = this.add
+          .text(this.screenCenterX, this.screenHeight / 2, 'Game Over', {
+            fontSize: '32px',
+            color: 'red',
+          })
+          .setOrigin(0.5, 0.5);
+
+        this.input.on('pointerup', () => {
+          this.score = 0;
+          PhaserSingletonService.destroyActiveGame();
+        });
+      },
+      undefined,
+      this
+    );
+  }
+
+  create() {
+    this.initValues();
+
+    this.addPlayerAnimation();
+
+    // adds collider between player and platforms
+    this.physics.add.collider(this.player, this.platform);
+
+    this.addPlayerMoves();
+
+    this.addStars();
+    this.addBooms();
 
     // Adds colliders between stars and bombs with platform
-
     this.physics.add.collider(
       this.stars,
       this.platform,
@@ -172,54 +256,9 @@ export class MainScene extends Scene {
       }
     );
 
-    // Adds overlap between player and stars
-    this.score = 0;
-    this.scoreText = this.add
-      .text(this.screenCenterX, this.gameAreaHeight + 16, 'Score: 0', {
-        fontSize: '16px',
-        color: '#000',
-      })
-      .setOrigin(0.5, 0.5);
+    this.playerAndStars();
 
-    this.physics.add.overlap(
-      this.player,
-      this.stars,
-      (object1: any, object2: any) => {
-        const star = object1.key === 'player' ? object1 : object2;
-        star.destroy();
-        this.score += 10;
-        this.scoreText.setText('Score: ' + this.score);
-      },
-      undefined,
-      this
-    );
-
-    // Adds overlap between player and bombs
-    this.physics.add.overlap(
-      this.player,
-      this.bombs,
-      (object1: any, object2: any) => {
-        const bomb = object1.key === 'player' ? object1 : object2;
-        bomb.destroy();
-        createStarLoop.destroy();
-        createBombLoop.destroy();
-        this.physics.pause();
-
-        this.gameOverText = this.add
-          .text(this.screenCenterX, this.screenHeight / 2, 'Game Over', {
-            fontSize: '32px',
-            color: 'red',
-          })
-          .setOrigin(0.5, 0.5);
-
-        this.input.on('pointerup', () => {
-          this.score = 0;
-          PhaserSingletonService.destroyActiveGame();
-        });
-      },
-      undefined,
-      this
-    );
+    this.playerAndBooms();
   }
 
   override update() {
