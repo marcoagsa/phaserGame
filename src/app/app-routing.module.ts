b1e1b16a2d57342b25b131e9a51faede31b30dc0
Marco Sa <marco.sa@md3.pt>
2023-12-03 11:12:58 +0000

    continue update project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,35 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    loadComponent: () => import('./tabs/tabs.page').then((m) => m.TabsPage),
+    loadChildren: () => [
+      {
+        path: 'about',
+        loadComponent: () =>
+          import('./tabs/about/about.page').then((m) => m.AboutPage),
+      },
+      {
+        path: 'play',
+        loadComponent: () =>
+          import('./tabs/play/play.page').then((m) => m.PlayPage),
+      },
+      {
+        path: 'scores',
+        loadComponent: () =>
+          import('./tabs/scores/scores.page').then((m) => m.ScoresPage),
+      },
+      {
+        path: '',
+        redirectTo: './tabs/about',
+        pathMatch: 'full',
+      },
+    ],
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
