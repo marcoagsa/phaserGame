a954ed2cc5b3fa7b8dc5f91c67f28a5dfe2165c7
Marco Sa <marco.sa@md3.pt>
2024-01-04 11:55:55 +0000

    add live reload to test on the devices

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,8 @@ const config: CapacitorConfig = {
   appName: 'phaserGame',
   webDir: 'www',
   server: {
+    url: 'http://192.168.1.19:8100',
+    cleartext: true,
     androidScheme: 'https',
   },
 };
