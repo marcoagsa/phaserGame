f987c120702e32d29c7508c1942d0c5f8c536983
Marco Sa <marco.sa@md3.pt>
2024-01-08 19:08:17 +0000

    improve code on background class

--- src/app/game/ui/background/background.ts
+++ src/app/game/ui/background/background.ts
@@ -2,31 +2,41 @@ import { BACKGROUND_ASSET_KEYS, PLATFORM_ASSET_KEYS } from 'src/app/constants';
 
 export class Background {
   #scene: Phaser.Scene;
-  background!: Phaser.Physics.Arcade.Image;
+  #background!: Phaser.Physics.Arcade.Image;
   platform!: Phaser.Types.Physics.Arcade.ImageWithStaticBody;
 
   constructor(scene: Phaser.Scene) {
     this.#scene = scene;
-    this.initBackground();
-    this.initPlatform();
+    this.#initBackground();
+    this.#initPlatform();
   }
 
-  initBackground() {
-    this.background = this.#scene.physics.add.image(
+  #initBackground() {
+    this.#background = this.#scene.physics.add
+      .image(0, 0, BACKGROUND_ASSET_KEYS.BG_FOREST)
+      .setAlpha(0);
+    this.#background.displayHeight = this.#scene.scale.height;
+    this.#background.scaleX = this.#background.scaleY;
+
+    this.#background.y = this.#scene.scale.height / 2;
+    this.#background.x = this.#scene.scale.width / 2;
+
+    this.#background.x = this.#background.displayHeight * 0.5;
+  }
+
+  showBackground() {
+    this.#background.setAlpha(1);
+  }
+
+  updateBackground() {
+    this.#background = this.#scene.physics.add.image(
       0,
       0,
-      BACKGROUND_ASSET_KEYS.BACKGROUND
+      BACKGROUND_ASSET_KEYS.BG_TOXIC
     );
-    this.background.displayHeight = this.#scene.scale.height;
-    this.background.scaleX = this.background.scaleY;
-
-    this.background.y = this.#scene.scale.height / 2;
-    this.background.x = this.#scene.scale.width / 2;
-
-    this.background.x = this.background.displayHeight * 0.5;
   }
 
-  initPlatform() {
+  #initPlatform() {
     const controlsAreaHeight = this.#scene.scale.height * 0.2;
     const gameAreaHeight = this.#scene.scale.height - controlsAreaHeight;
     this.platform = this.#scene.physics.add
