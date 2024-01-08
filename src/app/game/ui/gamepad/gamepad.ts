eafe4801438c66cce352cb338ac670fe1b10d9af
Marco Sa <marco.sa@md3.pt>
2024-01-08 19:38:02 +0000

    improve code on the game pad class

--- src/app/game/ui/gamepad/gamepad.ts
+++ src/app/game/ui/gamepad/gamepad.ts
@@ -1,59 +1,85 @@
 import { GAME_PAD_DIRECTIONS } from 'src/app/constants';
 
 export class GamePad {
-  #scene: Phaser.Scene;
-  leftArrow!: Phaser.GameObjects.Image;
-  rightArrow!: Phaser.GameObjects.Image;
   moveLeft!: boolean;
   moveRight!: boolean;
+  #scene: Phaser.Scene;
+  #leftArrow!: Phaser.GameObjects.Image;
+  #rightArrow!: Phaser.GameObjects.Image;
+  #controlsAreaHeight: number;
+  #sceneHeight: number;
+  #sceneWidth: number;
+  #gameAreaHeight: number;
 
   constructor(scene: Phaser.Scene) {
     this.#scene = scene;
+    this.#sceneHeight = this.#scene.scale.height;
+    this.#sceneWidth = this.#scene.scale.width;
+    this.#controlsAreaHeight = this.#sceneHeight * 0.2 - 20;
+    this.#gameAreaHeight = this.#sceneHeight - this.#controlsAreaHeight;
     this.initGamePad();
     this.gamePadMoves();
   }
 
-  initGamePad() {
-    const controlsAreaHeight = this.#scene.scale.height * 0.2 - 20;
-    const gameAreaHeight = this.#scene.scale.height - controlsAreaHeight;
+  /**
+   * Function to init game pad
+   * @private
+   */
+  private initGamePad() {
+    this.#leftArrow = this.addPhaserImage(
+      0.1,
+      this.#gameAreaHeight,
+      GAME_PAD_DIRECTIONS.LEFT
+    );
 
-    this.leftArrow = this.#scene.add
-      .image(
-        this.#scene.scale.width * 0.1,
-        gameAreaHeight,
-        GAME_PAD_DIRECTIONS.LEFT
-      )
-      .setOrigin(0, 0)
-      .setInteractive()
-      .setDepth(2);
-    this.rightArrow = this.#scene.add
-      .image(
-        this.#scene.scale.width * 0.7,
-        gameAreaHeight,
-        GAME_PAD_DIRECTIONS.RIGHT
-      )
+    this.#rightArrow = this.addPhaserImage(
+      0.7,
+      this.#gameAreaHeight,
+      GAME_PAD_DIRECTIONS.RIGHT
+    );
+  }
+
+  /**
+   * Function to add new Phaser image
+   * @private
+   * @param {number} x position of the image
+   * @param {number} y position of the image
+   * @param {(string | Phaser.Textures.Texture)} texture image to add
+   * @return {*}  {Phaser.GameObjects.Image}
+   */
+  private addPhaserImage(
+    x: number,
+    y: number,
+    texture: string | Phaser.Textures.Texture
+  ): Phaser.GameObjects.Image {
+    return this.#scene.add
+      .image(this.#sceneWidth * x, y, texture)
       .setOrigin(0, 0)
       .setInteractive()
       .setDepth(2);
   }
 
-  gamePadMoves() {
+  /**
+   * Function to handle pointer Down and Up
+   * @private
+   */
+  private gamePadMoves() {
     this.moveLeft = false;
     this.moveRight = false;
 
-    this.leftArrow.on('pointerdown', () => {
+    this.#leftArrow.on('pointerdown', () => {
       this.moveLeft = true;
     });
 
-    this.leftArrow.on('pointerup', () => {
+    this.#leftArrow.on('pointerup', () => {
       this.moveLeft = false;
     });
 
-    this.rightArrow.on('pointerdown', () => {
+    this.#rightArrow.on('pointerdown', () => {
       this.moveRight = true;
     });
 
-    this.rightArrow.on('pointerup', () => {
+    this.#rightArrow.on('pointerup', () => {
       this.moveRight = false;
     });
   }
