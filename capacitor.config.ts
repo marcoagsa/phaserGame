e9f90621f0f3398b30f5c2e62e0c91ad22f8015f
Marco Sa <marco.sa@md3.pt>
2024-02-10 21:39:33 +0000

    update libs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,13 +2,13 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.msa.phaserGame',
-  appName: 'phaserGame',
+  appName: 'PhaserGame',
   webDir: 'www/browser',
-  server: {
-    url: 'http://192.168.1.19:8100',
-    cleartext: true,
-    androidScheme: 'https',
-  },
+  // server: {
+  //   url: 'http://192.168.1.19:8100',
+  //   cleartext: true,
+  //   androidScheme: 'https',
+  // },
 };
 
 export default config;
